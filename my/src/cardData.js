333b8620664ea9ed7e29a7f1e0a67e5abf45617d
arodidev <51090527+arodidev@users.noreply.github.com>
2022-05-24 12:38:10 +0300

    Update cardData.js

--- my/src/cardData.js
+++ my/src/cardData.js
@@ -1,52 +1,51 @@
 export default [
   {
-    id:1,
+    id: 1,
     img: "maldives.jpg",
     country: "MALDIVES",
     gmaps:
-      "https://www.google.com/maps/d/embed?mid=17WqbESajFnu9bAeZDwwjZg23xdE&ehbc=2E312F",
+    "https://www.google.com/maps/d/embed?mid=1ew-z2xjgOgRizX6vdQhrl9Qu7hI&ehbc=2E312F" ,
     city: "Male",
     date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:"Definitely a sight to behold!",
+    description: "Definitely a sight to behold!",
   },
   {
-    id:2,
+    id: 2,
     img: "maasai.jpg",
     country: "KENYA",
     gmaps:
-      "https://www.google.com/maps/d/embed?mid=17WqbESajFnu9bAeZDwwjZg23xdE&ehbc=2E312F",
-    city: "Maasai Mara",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:"Definitely a sight to behold!",
+    "https://www.google.com/maps/d/embed?mid=1AAZdorO8k38q--f_8JJRuBegSMg&hl=en&ehbc=2E312F",    date: "12 Jan, 2021 - 24 Jan, 2021",
+    city:"Maasai Mara",
+    description: "Definitely a sight to behold!",
   },
   {
-    id:3,
+    id: 3,
     img: "santorini.jpg",
     country: "GREECE",
     gmaps:
-      "https://www.google.com/maps/d/embed?mid=17WqbESajFnu9bAeZDwwjZg23xdE&ehbc=2E312F",
+      "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiDyt7M_er3AhXwgf0HHbjGAYIQFnoECAwQAQ&url=https%3A%2F%2Fmaps.google.com%2Fmaps%2Fms%3Fie%3DUTF8%26oe%3DUTF8%26msa%3D0%26msid%3D103183963259253216666.0004944cae957a260acc1&usg=AOvVaw1_USWeOKGKvg65jJKkCYTO",
     city: "Santorini",
     date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:"Definitely a sight to behold!",
+    description: "Definitely a sight to behold!",
   },
   {
-    id:4,
+    id: 4,
     img: "monaco.webp",
     country: "MONACO",
     gmaps:
       "https://www.google.com/maps/d/embed?mid=17WqbESajFnu9bAeZDwwjZg23xdE&ehbc=2E312F",
     city: "Monaco",
     date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:"Definitely a sight to behold!",
+    description: "Definitely a sight to behold!",
   },
   {
-    id:5,
+    id: 5,
     img: "beirut.jpg",
     country: "LEBANON",
     gmaps:
       "https://www.google.com/maps/d/embed?mid=17WqbESajFnu9bAeZDwwjZg23xdE&ehbc=2E312F",
     city: "Beirut",
     date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:"Definitely a sight to behold!"
+    description: "Definitely a sight to behold!",
   },
 ];
